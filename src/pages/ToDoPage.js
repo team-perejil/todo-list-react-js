81b5f1a3050d680b45f62f91c518e975eca3d8ea
Andrea Martinez <andrea.bitmind@gmail.com>
2023-04-24 15:12:27 +0200

    fix: TodoForm route name changed

--- src/pages/ToDoPage.js
+++ src/pages/ToDoPage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import TodoForm from "../Components/Form";
+import TodoForm from "../Components/TodoForm";
 
 const ToDoPage = () => {
   const initialmockToDo = [
