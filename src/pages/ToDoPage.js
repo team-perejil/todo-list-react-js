8c836145064fe5aefc2ef04597916b78f481efaf
Andrea Martinez <andrea.bitmind@gmail.com>
2023-04-24 15:09:47 +0200

    feat: added state to mock list and TodoForm component

--- src/pages/ToDoPage.js
+++ src/pages/ToDoPage.js
@@ -1,23 +1,35 @@
+import { useState } from "react";
+import TodoForm from "../Components/Form";
+
 const ToDoPage = () => {
+  const initialmockToDo = [
+    { title: "myFirstToDo" },
+    { title: "myFirstToDo" },
+    { title: "myFirstToDo" },
+  ];
+
+  const [mockTodo, setMockTodo] = useState(initialmockToDo);
+
+  const handlerAddItem = (newItem) => {
+    setMockTodo([...mockTodo, newItem]);
+  };
+
   return (
-    <div>
-      {mockToDo.map((toDo) => {
-        return <div>{toDo.title}</div>;
-      })}
-    </div>
+    <>
+      <div>
+        {mockTodo.map((toDo) => {
+          return <div>{toDo.title}</div>;
+        })}
+      </div>
+      <TodoForm onAddItem={handlerAddItem} />
+    </>
   );
 };
-
-const mockToDo = [
-  { title: "myFirstToDo" },
-  { title: "myFirstToDo" },
-  { title: "myFirstToDo" },
-];
 
 export default ToDoPage;
 
 /* queremos añadir ítems al mockList
-1) Basico: queremos un formulario con un input con un boton que le hagas un push al array
+1) Basico: queremos un formulario con un input con un boton que le hagas un push al array --> OK
 2) el objetivo es que cada input (cada todo) sea editable:
 - en lugar de divs queremos que sean inputs
 - a medida que voy escribiendo se vaya actualizando el mockToDos  */
