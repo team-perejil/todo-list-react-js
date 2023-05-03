e3acfe631085c042355203c9956f35f1107b6c74
Pablo S <sobrinopablo@gmail.com>
2023-05-03 11:22:34 +0200

    not my day

--- src/API/API.js
+++ src/API/API.js
@@ -3,3 +3,5 @@ export const readItems = () => {
   const getItems = localStorage.getItem(LOCAL_STORAGE_KEY);
   return getItems;
 };
+
+// Acabar el Read y vamos viendo si eso (Create como ambición) acordarnos de parsear
