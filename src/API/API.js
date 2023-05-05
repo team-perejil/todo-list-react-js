b4c995439327438112eff5a85e828eb68c1a61c5
Andrea Martinez <andrea.maort@gmail.com>
2023-05-05 11:19:39 +0200

    feat: added deleteItems function in API

--- src/API/API.js
+++ src/API/API.js
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
 const LOCAL_STORAGE_KEY = "items";
-const DEFAULT_ITEM = { id: "default_id", title: "Default item" };
 
 export const readItems = () => {
   const getItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
@@ -13,10 +12,15 @@ export const readItems = () => {
 export const addItems = (item) => {
   const id = uuidv4();
   const newItem = { id, ...item };
-  const items = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [
-    DEFAULT_ITEM,
-  ];
+  const items = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
   const newItems = items.length > 0 ? [...items, newItem] : [newItem];
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newItems));
   return newItems;
+};
+
+export const deteleItems = (id) => {
+  const items = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+  const deleteItem = items.filter((item) => item.id !== id);
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(deleteItem));
+  return deleteItem;
 };
